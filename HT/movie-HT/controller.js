1ea40af844ade6b500f7ebfdf410231768a6012f
Willyb15 <william.steen.bryant@gmail.com>
2016-05-28 22:43:01 -0400

    movie app in angular

---
+++ HT/movie-HT/controller.js
@@ -0,0 +1,40 @@
+var movieApp = angular.module('movieApp', []);
+// var apiUrl = 'http://localhost:3020/';
+movieApp.controller('movieController', function($scope, $http) {
+	// $scope.message = "hello world";
+	var movieURL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=5e2170b9fb801a61d6d784870c4c2eb1';
+	var api_key = 'api_key=5e2170b9fb801a61d6d784870c4c2eb1';
+	$scope.imagePath = "https://image.tmdb.org/t/p/w300";
+
+		
+	$http({
+		method: 'GET',
+		url: movieURL,
+	}).then(function(movieData){
+		$scope.movieData = movieData.data.results;
+			console.log(movieData);
+	}, function(movieData){
+		console.log('There was an  error!!');
+	});
+
+	$scope.movieSearch = function(){
+		console.log($scope.searchTerm);
+		console.log($scope.selected);
+		if ($scope.selected == 'person'){
+			searchURL = 'https://api.themoviedb.org/3/search/person?search_type=ngram&query=' + encodeURI($scope.searchTerm) + '&' + api_key;
+		}else{
+			searchURL = 'https://api.themoviedb.org/3/search/movie?api_key=5e2170b9fb801a61d6d784870c4c2eb1&query='+encodeURI($scope.searchTerm);
+		}
+		$http({
+			method: 'GET',
+			url: searchURL,
+		}).then(function(movieData){
+			$scope.movieData = movieData.data.results;
+				console.log(movieData);
+		}, function(movieData){
+			console.log('There was an  error!!');
+		});
+	};
+
+
+});
